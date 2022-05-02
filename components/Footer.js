8ab112daa5213b16ca2fe323729d7cd051e2dad7
tomohdie <tomohidex@gmail.com>
2022-05-02 21:02:10 +0900

    to typescript add prettier format

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,9 @@
-import styles from './Footer.module.css'
-
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+      <footer>
+        Made with <img src="/netliheart.svg" alt="Netlify Logo" /> for you
       </footer>
     </>
-  )
+  );
 }
